7128c043b8465b8118031f8a68079596f17dfaa1
Valcuda <themodhead@gmail.com>
2022-03-23 12:32:18 -0400

    crud

--- Game files/Sweet Mems GD/Rand Level Gen/Prefabs/Prefabs_List.gd
+++ Game files/Sweet Mems GD/Rand Level Gen/Prefabs/Prefabs_List.gd
@@ -4,7 +4,14 @@ var pref2 : String
 
 var prefabs : Array = ["res://Rand Level Gen/Prefabs/Gas Challenge.tscn", "res://Rand Level Gen/Prefabs/Brandin.tscn"]
 
+var jigsaw_loc = Vector2.ZERO
+
+signal jigsaw_move
+
 func rand_prefab():
 	pref = FreeAccessInfo.RN_Jesus(1, 0, prefabs.size())
 	pref2 = prefabs[pref]
 	return(pref2)
+
+func signal_it():
+	emit_signal("jigsaw_move")
